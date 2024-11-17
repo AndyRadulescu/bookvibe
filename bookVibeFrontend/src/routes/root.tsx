47cc974a59bafa1c4c15ce5ed8435d2f2f1381ed
Andy Radulescu <radulescu.eduard.andrei@gmail.com>
2024-11-17 12:01:17 +0200

    extend serac Volume list

--- bookVibeFrontend/src/routes/root.tsx
+++ bookVibeFrontend/src/routes/root.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import DashboardPage from './dashboard/dashboard-page.tsx';
 
 export default function Root() {
--- worker/src/main/kotlin/org/example/worker/dto/SearchVolumeList.kt
+++ worker/src/main/kotlin/org/example/worker/dto/SearchVolumeList.kt
@@ -1,5 +1,7 @@
 package org.example.worker.dto
 
+import java.util.*
+
 data class SearchVolumeList(val kind: String, val totalItems: Int, val items: List<VolumeDto>) {
     data class VolumeDto(
             val kind: String,
@@ -8,37 +10,109 @@ data class SearchVolumeList(val kind: String, val totalItems: Int, val items: Li
             val selfLink: String,
             val volumeInfo: VolumeInfoDto,
             val saleInfo: SaleInfoDto,
+            val userInfo: UserInfo?,
             val accessInfo: AccessInfoDto,
-            val searchInfo: SearchInfoDto)
+            val searchInfo: SearchInfoDto) {
 
-    data class VolumeInfoDto(
-            val title: String,
-            val authors: List<String>,
-            val publishedDate: String,
-            val description: String,
-            val industryIdentifiers: List<IndustryIdentifiersDto>?,
-            val readingModes: ReadingModesDto,
+        data class VolumeInfoDto(
+                val title: String,
+                val subtitle: String?,
+                val authors: List<String>,
+                val publisher: String?,
+                val publishedDate: String,
+                val description: String?,
+                val industryIdentifiers: List<IndustryIdentifiersDto>,
+                val readingModes: ReadingModesDto?,
+                val pageCount: Number,
+                val dimensions: BookDimensions?,
+                val printType: String?,
+                val mainCategory: String?,
+                val categories: List<String>?,
+                val averageRating: Number?,
+                val ratingCount: Number?,
+                val contentVersion: String?,
+                val imageLinks: ImageLinks?,
+                val language: String,
+                val previewLink: String,
+                val infoLink: String,
+                val canonicalVolumeLink: String
+        ) {
+            data class ImageLinks(
+                    val smallThumbnail: String?,
+                    val thumbnail: String?,
+                    val small: String?,
+                    val medium: String?,
+                    val large: String?,
+                    val extraLarge: String?
+            )
+
+            data class IndustryIdentifiersDto(val type: String, val identifier: String)
+            data class ReadingModesDto(val text: Boolean, val image: Boolean)
+            data class BookDimensions(
+                    val height: String?,
+                    val width: String?,
+                    val thickness: String?
+            )
+        }
+
+        data class SaleInfoDto(
+                val country: String?,
+                val saleability: String?,
+                val isEbook: Boolean?,
+                val onSaleDate: Date?,
+                val listPrice: Price?,
+                val retailPrice: Price?,
+                val buyLink: String?
+        )
+
+        data class UserInfo(
+                val review: Any,
+                val readingPosition: Any,
+                val isPurchased: Boolean?,
+                val isPreordered: Boolean?,
+                val updated: Date?
+        )
+
+        data class AccessInfoDto(
+                val country: String,
+                val viewability: String,
+                val embeddable: Boolean,
+                val publicDomain: Boolean,
+                val textToSpeechPermission: String,
+                val epub: FormatDto,
+                val pdf: FormatDto,
+                val webReaderLink: String,
+                val accessViewStatus: String,
+                val quoteSharingAllowed: Boolean,
+                val downloadAccess: DownloadAccess?
+        ) {
+            data class FormatDto(
+                    val isAvailable: Boolean,
+                    val downloadLink: String?,
+                    val acsTokenLink: String?
+            )
+
+            data class DownloadAccess(
+                    val kind: String?,
+                    val volumeId: String?,
+                    val restricted: Boolean?,
+                    val deviceAllowed: Boolean?,
+                    val justAcquired: Boolean?,
+                    val maxDownloadDevices: Number?,
+                    val downloadsAcquired: Number?,
+                    val nonce: String?,
+                    val source: String?,
+                    val reasonCode: String?,
+                    val message: String?,
+                    val signature: String?
+            )
+        }
+
+        data class SearchInfoDto(val textSnippet: String)
+    }
+
+    data class Price(
+            val amount: Number,
+            val currency: Number
     )
-
-
-    data class SaleInfoDto(val country: String, val saleability: String, val isEbook: Boolean)
-
-    data class AccessInfoDto(
-            val country: String,
-            val viewability: String,
-            val embeddable: Boolean,
-            val publicDomain: Boolean,
-            val textToSpeechPermission: String,
-            val epub: IsAvailableDto,
-            val pdf: IsAvailableDto,
-            val webReaderLink: String,
-            val accessViewStatus: String,
-            val quoteSharingAllowed: Boolean
-    )
-
-    data class SearchInfoDto(val textSnippet: String)
-
-    data class IndustryIdentifiersDto(val type: String, val identifier: String)
-    data class ReadingModesDto(val text: Boolean, val image: Boolean)
-    data class IsAvailableDto(val isAvailable: Boolean)
 }
